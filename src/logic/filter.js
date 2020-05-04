4a344da77cce1a8b9279ef6bd3e68de9779d1871
Gosch <pro@gosch.io>
2020-05-04 03:03:28 -0300

    Fix regex of filter

--- src/logic/filter.js
+++ src/logic/filter.js
@@ -1,5 +1,5 @@
 export const filterByName = (dragons, name) => {
-  const filter = name.replace(/[^\w\s]/, '')
+  const filter = name.replace(/[^0-9a-zA-Z\s]+/, '')
   const reg = new RegExp(filter, 'i');
   return dragons.filter((dragon) => reg.test(dragon.name));
 };
